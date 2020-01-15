60acc229fd9122c26e8ee09634e5b13b2d147e71
Ayoub Ouddah <ayoub.ouddah@gmail.com>
2020-01-16 00:38:50 +0100

    fix the validation methode

--- src/eval.py
+++ src/eval.py
@@ -2,14 +2,12 @@ import torch
 import torch.nn.functional as F
 from tqdm import tqdm
 import numpy as np
-#from sklearn.metrics import accuracy_score
-#from sklearn.metrics import precision_score
-#from sklearn.metrics import jaccard_score
-#from sklearn.metrics import recall_score
-#from sklearn.metrics import f1_score
-#from sklearn.metrics import multilabel_confusion_matrix
 
-from utils.perMes import *
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import jaccard_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import f1_score
 from dice_loss import dice_coeff
 
 
@@ -17,6 +15,7 @@ def eval_net(net, loader, device, n_val):
     """Evaluation without the densecrf with the dice coefficient"""
     net.eval()
     tot = 0
+    jacc_score = 0
     acc_score = 0
     recall_score = 0
     jacc_score = 0
@@ -35,9 +34,10 @@ def eval_net(net, loader, device, n_val):
     net.eval()
     tot = 0
     acc_score = 0
-    recall_score = 0
+    rec_score = 0
     f1_score = 0
     pres_score = 0
+    jacc_score = 0
     confMat = np.zeros((2, 2))
     with tqdm(total=n_val, desc='Validation round', unit='img', leave=False) as pbar:
         for batch in loader:
@@ -57,22 +57,26 @@ def eval_net(net, loader, device, n_val):
                 else:
                     tot += dice_coeff(pred, true_mask.squeeze(dim=1)).item()
                     pred = pred.detach().cpu().numpy()
-                    #pred = np.reshape(pred, -1)
-                    pred = pred.astype(bool)
-                    true_mask = true_mask.cpu().numpy()
-                    #true_mask = np.reshape(true_mask, -1) 
-                    true_mask = true_mask.astype(bool)   
-                    for i in range(true_mask.shape[0]):
-                      for j in range(true_mask.shape[1]):
-                        confMat[int(true_mask[0, i, j]), int(pred[0, i, j])] += 1  
-                    #confMatrix  = multilabel_confusion_matrix(true_mask, pred)
-                    #print(confMatrix.shape)
+                    pred = pred.astype(int)
+                    pred = np.matrix.flatten(pred)
                    
+                    true_mask = true_mask.cpu().numpy()
+                    true_mask = true_mask.astype(int)   
+                    true_mask = np.matrix.flatten(true_mask)
+                   
+                    jacc_score += jaccard_score(true_mask, pred)
+                    acc_score += accuracy_score(true_mask, pred)
+                    pres_score += precision_score(true_mask, pred)
+                    rec_score += recall_score(true_mask, pred)
             pbar.update(imgs.shape[0])
-
-    acc_score = accuracy(confMat)
-    pres_score  = precision(confMat)
-    recall_score = recall(confMat)
-    f1_score = scoreF1(confMat)
-    return (tot / n_val), (acc_score / n_val), (pres_score / n_val), (recall_score / n_val), (f1_score / n_val)
+    tot = (tot / n_val)
+    jacc_score = (jacc_score / n_val)
+    acc_score = (acc_score / n_val)
+    pres_score = (pres_score / n_val)
+    rec_score = (rec_score / n_val)
+    if (pres_score + rec_score) > 0:
+      f1_score = 2 * (pres_score * rec_score) / (pres_score + rec_score)
+    else:
+      f1_score = 0
+    return tot, jacc_score, acc_score, pres_score, rec_score, f1_score
                     
