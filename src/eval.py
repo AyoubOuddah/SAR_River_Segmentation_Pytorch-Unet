ac0d329ade54ab9077e58ed1b5212736326a144e
Ayoub Ouddah <ayoub.ouddah@gmail.com>
2020-01-15 19:15:23 +0100

    and another one

--- src/eval.py
+++ src/eval.py
@@ -2,11 +2,14 @@ import torch
 import torch.nn.functional as F
 from tqdm import tqdm
 import numpy as np
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import precision_score
-from sklearn.metrics import jaccard_score
-from sklearn.metrics import recall_score
-from sklearn.metrics import f1_score
+#from sklearn.metrics import accuracy_score
+#from sklearn.metrics import precision_score
+#from sklearn.metrics import jaccard_score
+#from sklearn.metrics import recall_score
+#from sklearn.metrics import f1_score
+#from sklearn.metrics import multilabel_confusion_matrix
+
+from utils.perMes import *
 from dice_loss import dice_coeff
 
 
@@ -19,7 +22,7 @@ def eval_net(net, loader, device, n_val):
     jacc_score = 0
     f1_score = 0
     pres_score = 0
-    #confMatrix = np.zeros((2,2))
+    confMat = np.zeros((2,2))
 import torch
 import torch.nn.functional as F
 from tqdm import tqdm
@@ -33,9 +36,9 @@ def eval_net(net, loader, device, n_val):
     tot = 0
     acc_score = 0
     recall_score = 0
-    jacc_score = 0
     f1_score = 0
     pres_score = 0
+    confMat = np.zeros((2, 2))
     with tqdm(total=n_val, desc='Validation round', unit='img', leave=False) as pbar:
         for batch in loader:
             imgs = batch['image']
@@ -54,14 +57,22 @@ def eval_net(net, loader, device, n_val):
                 else:
                     tot += dice_coeff(pred, true_mask.squeeze(dim=1)).item()
                     pred = pred.detach().cpu().numpy()
-                    pred = np.reshape(pred, -1)
+                    #pred = np.reshape(pred, -1)
+                    pred = pred.astype(bool)
                     true_mask = true_mask.cpu().numpy()
-                    true_mask = np.reshape(true_mask, -1)           
-                    acc_score += accuracy_score(true_mask, pred)
-                    pres_score += precision_score(true_mask, pred)
-                    jacc_score += jaccard_score(true_mask, pred)
-                    recall_score += recall_score(true_mask, pred)
-                    f1_score += f1_score(true_mask, pred)
+                    #true_mask = np.reshape(true_mask, -1) 
+                    true_mask = true_mask.astype(bool)   
+                    for i in range(true_mask.shape[0]):
+                      for j in range(true_mask.shape[1]):
+                        confMat[int(true_mask[0, i, j]), int(pred[0, i, j])] += 1  
+                    #confMatrix  = multilabel_confusion_matrix(true_mask, pred)
+                    #print(confMatrix.shape)
+                   
             pbar.update(imgs.shape[0])
-    return (tot / n_val), (acc_score / n_val), (pres_score / n_val), (jacc_score/ n_val), (recall_score / n_val), (f1_score / n_val)
+
+    acc_score = accuracy(confMat)
+    pres_score  = precision(confMat)
+    recall_score = recall(confMat)
+    f1_score = scoreF1(confMat)
+    return (tot / n_val), (acc_score / n_val), (pres_score / n_val), (recall_score / n_val), (f1_score / n_val)
                     
