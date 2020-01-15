9887a4878ba2e5785c9cd179a09fa8f2a30f7a60
Ayoub Ouddah <ayoub.ouddah@gmail.com>
2020-01-15 18:37:38 +0100

    fix bugs

--- src/eval.py
+++ src/eval.py
@@ -1,7 +1,12 @@
 import torch
 import torch.nn.functional as F
 from tqdm import tqdm
-import sklearn
+import numpy as np
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import jaccard_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import f1_score
 from dice_loss import dice_coeff
 
 
@@ -15,31 +20,48 @@ def eval_net(net, loader, device, n_val):
     f1_score = 0
     pres_score = 0
     #confMatrix = np.zeros((2,2))
+import torch
+import torch.nn.functional as F
+from tqdm import tqdm
+
+from dice_loss import dice_coeff
+
+
+def eval_net(net, loader, device, n_val):
+    """Evaluation without the densecrf with the dice coefficient"""
+    net.eval()
+    tot = 0
+    acc_score = 0
+    recall_score = 0
+    jacc_score = 0
+    f1_score = 0
+    pres_score = 0
     with tqdm(total=n_val, desc='Validation round', unit='img', leave=False) as pbar:
         for batch in loader:
             imgs = batch['image']
             true_masks = batch['mask']
+
             imgs = imgs.to(device=device, dtype=torch.float32)
-            true_mask = true_mask.to(device=device, dtype=torch.float32)
+            mask_type = torch.float32 if net.n_classes == 1 else torch.long
+            true_masks = true_masks.to(device=device, dtype=mask_type)
+
             mask_pred = net(imgs)
-            mask_pred = (mask_pred > 0.5).int()
-            for idx, true_mask in enumerate(true_masks):
-                mask_pred = (mask_pred > 0.5).float()
+
+            for true_mask, pred in zip(true_masks, mask_pred):
+                pred = (pred > 0.5).float()
                 if net.n_classes > 1:
-                    tot += F.cross_entropy(mask_pred.unsqueeze(dim=0), true_mask.unsqueeze(dim=0)).item()
+                    tot += F.cross_entropy(pred.unsqueeze(dim=0), true_mask.unsqueeze(dim=0)).item()
                 else:
-                    tot += dice_coeff(mask_pred, true_mask.squeeze(dim=1)).item()
-
-            mask_pred = mask_pred.detach().cpu().numpy()
-            true_mask = mask_pred.cpu().numpy()
-            print(mask_pred.shape)
-            print(true_mask.shape)
-            acc_score += sklearn.metrics.accuracy_score(true_mask, mask_pred)
-            pres_score += sklearn.metrics.precision_score(true_mask, mask_pred)
-            jacc_score += sklearn.metrics.jaccard_score(true_mask, mask_pred)
-            recall_score += sklearn.metrics.recall_score(true_mask, mask_pred)
-            f1_score += sklearn.metrics.f1_score(true_mask, mask_pred)
-
+                    tot += dice_coeff(pred, true_mask.squeeze(dim=1)).item()
+                    pred = pred.detach().cpu().numpy()
+                    pred = np.reshape(pred, -1)
+                    true_mask = true_mask.cpu().numpy()
+                    true_mask = np.reshape(true_mask, -1)           
+                    acc_score += accuracy_score(true_mask, pred)
+                    pres_score += precision_score(true_mask, pred)
+                    jacc_score += jaccard_score(true_mask, pred)
+                    recall_score += recall_score(true_mask, pred)
+                    f1_score += f1_score(true_mask, pred)
             pbar.update(imgs.shape[0])
-
     return (tot / n_val), (acc_score / n_val), (pres_score / n_val), (jacc_score/ n_val), (recall_score / n_val), (f1_score / n_val)
+                    
